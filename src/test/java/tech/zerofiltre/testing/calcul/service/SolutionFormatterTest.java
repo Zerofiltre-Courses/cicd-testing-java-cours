b24398c7ec2d54dba4accb669aa8582ca64e3dfe
AxelGROUSSAUD <70748003+AxelGROUSSAUD@users.noreply.github.com>
2024-06-27 12:23:10 +0200

    Update SolutionFormatterTest.java

--- src/test/java/tech/zerofiltre/testing/calcul/service/SolutionFormatterTest.java
+++ src/test/java/tech/zerofiltre/testing/calcul/service/SolutionFormatterTest.java
@@ -17,13 +17,13 @@ public class SolutionFormatterTest {
 	@Test
 	public void format_shouldFormatAnyBigNumber() {
 		// GIVEN
-		final int number = 1234567890;
+		final int number = 1;
 
 		// WHEN
 		final String result = solutionFormatter.format(number);
 
 		// THEN
-		assertThat(result).isEqualTo("1 234 567 890");
+		assertThat(result).isEqualTo("1");
 	}
 
 }
